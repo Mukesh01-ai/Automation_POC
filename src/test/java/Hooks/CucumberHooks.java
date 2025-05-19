a7bcbf34d096d7e49c9e35b9b0db4ebaf362a734
Mukesh01-ai <mukeshktester@gmail.com>
2025-05-19 19:33:30 +0530

    Runner file

--- src/test/java/Hooks/CucumberHooks.java
+++ src/test/java/Hooks/CucumberHooks.java
@@ -4,6 +4,7 @@ import io.cucumber.java.Before;
 import static io.restassured.RestAssured.baseURI;
 
 public class CucumberHooks {
+	
     @Before
     public  void setup() {
         baseURI="http://127.0.0.1:8000";
